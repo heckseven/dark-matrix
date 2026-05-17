4c73e27787a0eeb9310bb918223066f758cf1734
heckseven <heckseven@proton.me>
2026-05-17 09:28:07 -0600

    fix: wrap AudioLab in TooltipProvider

--- src/designer/web/main.tsx
+++ src/designer/web/main.tsx
@@ -3,11 +3,12 @@ import { createRoot } from 'react-dom/client';
 import './globals.css';
 import { App } from './App.js';
 import { AudioLab } from './components/AudioLab.js';
+import { TooltipProvider } from './components/ui/tooltip.js';
 
 const isLab = new URLSearchParams(window.location.search).has('lab');
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {isLab ? <AudioLab /> : <App />}
+    {isLab ? <TooltipProvider><AudioLab /></TooltipProvider> : <App />}
   </StrictMode>,
 );
